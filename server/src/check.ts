2a3be5968004d07262c7dce9ba30129e7b81f196
bhagyeshmagar <bhageshgmagar1@gmail.com>
2026-06-09 04:47:30 +0530

    chore: remove debug scripts and txt files

--- server/src/check.ts
+++
@@ -1,9 +0,0 @@
-import { db } from './server/src/db';
-import { coreTeamRoles } from './server/src/db/schema';
-import { eq } from 'drizzle-orm';
-async function run() {
-    const roles = await db.select().from(coreTeamRoles).where(eq(coreTeamRoles.id, 129));
-    console.log(roles);
-    process.exit(0);
-}
-run();
